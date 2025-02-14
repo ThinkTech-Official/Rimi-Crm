a452b57c47c8dc99f521c8a8c39069e13f54fe2a
ThinkTech <tech@thethinktech.com>
2025-02-14 19:51:20 +0530

    added createUser component and Document component

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -1,7 +1,63 @@
 
 
-export default function CreateUser() {
+
+import React, { useState } from "react";
+import {
+  EyeIcon,
+  EyeSlashIcon
+} from '@heroicons/react/24/outline'
+
+
+const CreateUser: React.FC = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
   return (
-    <div>CreateUser</div>
-  )
-}
+    <div className="max-w-3xl mx-auto mt-10 p-6  rounded-lg shadow-2xl">
+      <h2 className="text-xl font-semibold text-center text-[#3a17c5] mb-2">CREATE USER</h2>
+      <p className="text-center text-gray-600 mb-4">** Changes to User Type will restore User Permissions to default settings **</p>
+      
+      <div className="grid grid-cols-3 gap-4 text-gray-700">
+        <div className="col-span-3 text-[#3a17c5] font-semibold">USER INFORMATION</div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">First Name</label><input className="p-2 border rounded" placeholder="FIRST NAME" /></div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">Last Name</label><input className="p-2 border rounded" placeholder="LAST NAME" /></div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">Email</label><input className="p-2 border rounded" type="email" placeholder="EMAIL" /></div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">Agent Code</label><input className="p-2 border rounded" placeholder="AGENT CODE" /></div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">Company</label><input className="p-2 border rounded" placeholder="COMPANY" /></div>
+        <div className=" flex flex-col gap-2"><label htmlFor="">User Type</label><select className="p-2 border rounded">
+          <option>---</option>
+          <option>Admin</option>
+          <option>Mga</option>
+          <option>Agent</option>
+          <option>Read Only</option>
+        </select>
+        </div>
+        <div className="col-span-1 space-y-2">
+          <label className="block text-gray-700">STATUS</label>
+          <div className="flex space-x-4">
+            <label><input type="radio" name="status" value="active" /> Active</label>
+            <label><input type="radio" name="status" value="inactive" /> Inactive</label>
+          </div>
+        </div>
+        <div className="col-span-2 space-y-4">
+          <label className="block text-gray-700">PASSWORD</label>
+          <div className="relative">
+            <input type={passwordVisible ? "text" : "password"} className="w-full p-2 border rounded" placeholder="New Password" />
+            <span className="absolute right-3 top-3 cursor-pointer" onClick={() => setPasswordVisible(!passwordVisible)}>{passwordVisible ? <EyeIcon className="h-4 w-4" aria-hidden="true" /> : <EyeSlashIcon className="h-4 w-4" aria-hidden="true" />}</span>
+          </div>
+          <div className="relative mt-2">
+            <input type={confirmPasswordVisible ? "text" : "password"} className="w-full p-2 border rounded" placeholder="Re-enter Password" />
+            <span className="absolute right-3 top-3 cursor-pointer" onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}>{confirmPasswordVisible ? <EyeIcon className="h-4 w-4" aria-hidden="true" /> : <EyeSlashIcon className="h-4 w-4" aria-hidden="true" />}</span>
+          </div>
+        </div>
+      </div>
+      <div className=" w-full flex justify-center items-center mt-2">
+      <button className="w-[250px] mt-4 bg-[#3a17c5] text-white p-2 rounded hover:bg-[#3a17c5e8] transition flex justify-center items-center cursor-pointer">
+        CREATE USER
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
--- src/components/Documents.tsx
+++ src/components/Documents.tsx
@@ -1,7 +1,42 @@
 
 
+import {
+  PencilSquareIcon,
+  TrashIcon,
+  FolderPlusIcon
+} from '@heroicons/react/24/outline'
+
+const demoDocuments =[
+  {id: 1, name: 'RIMI Canuck Voyage Travel Medical - Claim Form (EN)'},
+  {id: 2, name: 'RIMI Canuck Voyage Travel Medical - Claim Form (FR)'},
+  {id: 3, name: 'Secure Study RIMI International Students to Canada - Policy Wording (EN)'},
+  {id: 4, name: 'Secure Study RIMI International Students to Canada - Policy Wording (FR)'},
+]
+
+
 export default function Documents() {
   return (
-    <div>Documents</div>
+    <div className="max-w-4xl mx-auto mt-10">
+      <h2 className="text-xl font-semibold text-center text-[#3a17c5] mb-4">PRODUCTS</h2>
+      <div className=" max-w-4xl mt-10">
+
+        {demoDocuments.map((item) => (
+          <div className=" flex justify-between items-center border-b py-2 border-slate-200 w-full">
+          {item.name}
+          <div className='flex justify-center items-center gap-2'>
+          <button className=" px-2 py-2 border border-slate-400 shadow-2xl flex gap-2 cursor-pointer"><PencilSquareIcon className="h-6 w-6" aria-hidden="true" /> </button>
+          <button className=" px-2 py-2 border border-slate-400 shadow-2xl flex gap-2 cursor-pointer"><TrashIcon className="h-6 w-6" aria-hidden="true" /> </button>
+          </div>
+      </div>
+        ))}
+
+        
+
+      </div>
+
+      <div className=' mt-5 flex justify-center items-center'>
+      <button className=" px-2 py-2 border border-slate-400 shadow-2xl flex gap-2 cursor-pointer"><FolderPlusIcon className="h-6 w-6" aria-hidden="true" /> ADD </button>
+      </div>
+    </div>
   )
 }
