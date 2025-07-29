277b489b373d09f38d62030e57dc089fbc98dd42
dheerajttt <dheeraj@thethinktech.com>
2025-07-29 19:18:53 +0530

    added  validation on search quotes form

--- src/components/PoliciesSearch.tsx
+++ src/components/PoliciesSearch.tsx
@@ -7,6 +7,7 @@ import {
 } from "../hooks/useSearchPolicies";
 import { Link, useNavigate } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 const allProducts = [
   "RIMI Canuck Voyage Travel Medical",
@@ -28,6 +29,20 @@ const PoliciesSearch: React.FC = () => {
   const [page, setPage] = useState(1);
   const limit = 10;
   const { search, loading, error, data } = useSearchPolicies(limit);
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<SearchPoliciesCriteria>({
+    defaultValues: {
+      products: ["All"],
+    },
+  });
+  useEffect(() => {
+    register("status", { required: "Status is required" });
+  }, [register]);
 
   const handleChange =
     (key: keyof Omit<SearchPoliciesCriteria, "page" | "limit">) =>
@@ -47,9 +62,10 @@ const PoliciesSearch: React.FC = () => {
     });
   };
 
-  const onSearch = () => {
+  const onSearch = (formData: SearchPoliciesCriteria) => {
+    console.log(formData);
     setPage(1);
-    search(criteria, 1, limit);
+    search(formData, 1, limit);
   };
 
   const goToPage = (p: number) => {
@@ -105,134 +121,228 @@ const PoliciesSearch: React.FC = () => {
       </div> */}
 
       {/*  */}
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 md:gap-x-16 lg:gap-x-24 gap-y-4 text-text-secondary">
-        {/* First Name */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">First Name</label>
-          <input
-            value={criteria.firstName || ""}
-            onChange={handleChange("firstName")}
-            className="input-primary"
-            placeholder="First Name"
-          />
-        </div>
-        {/* Last Name */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Last Name</label>
-          <input
-            value={criteria.lastName || ""}
-            onChange={handleChange("lastName")}
-            className="input-primary"
-            placeholder="Last Name"
-          />
-        </div>
-        {/* Date of Birth */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Date of Birth</label>
-          <input
-            type="date"
-            value={criteria.dateOfBirth || ""}
-            onChange={handleChange("dateOfBirth")}
-            className="input-primary"
-            placeholder="Date of Birth"
-          />
-        </div>
-        {/* Policy Number */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Policy Number</label>
-          <input
-            value={criteria.policyNumber || ""}
-            onChange={handleChange("policyNumber")}
-            className="input-primary"
-            placeholder="Policy Number"
-          />
-        </div>
-        {/* Phone Number */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Phone Number</label>
-          <input
-            value={criteria.phoneNumber || ""}
-            onChange={handleChange("phoneNumber")}
-            className="input-primary"
-            placeholder="Phone Number"
-          />
-        </div>
-        {/* Email */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Email</label>
-          <input
-            type="email"
-            value={criteria.email || ""}
-            onChange={handleChange("email")}
-            className="input-primary"
-            placeholder="Email"
-          />
-        </div>
-        {/* Sale Date From */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Sale Date From</label>
-          <input
-            type="date"
-            value={criteria.saleDateFrom || ""}
-            onChange={handleChange("saleDateFrom")}
-            className="input-primary"
-          />
-        </div>
-        {/* Sale Date To */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Sale Date To</label>
-          <input
-            type="date"
-            value={criteria.saleDateTo || ""}
-            onChange={handleChange("saleDateTo")}
-            className="input-primary"
-          />
-        </div>
-        {/* Effective Date From */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Effective Date From</label>
-          <input
-            type="date"
-            value={criteria.effectiveDateFrom || ""}
-            onChange={handleChange("effectiveDateFrom")}
-            className="input-primary"
-          />
-        </div>
-        {/* Effective Date To */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Effective Date To</label>
-          <input
-            type="date"
-            value={criteria.effectiveDateTo || ""}
-            onChange={handleChange("effectiveDateTo")}
-            className="input-primary"
-          />
-        </div>
-        {/* Application ID */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Application ID</label>
-          <input
-            value={criteria.applicationId || ""}
-            onChange={handleChange("applicationId")}
-            className="input-primary"
-            placeholder="Application ID"
-          />
-        </div>
-        {/* Agent */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Agent</label>
-          <input
-            value={criteria.agent || ""}
-            onChange={handleChange("agent")}
-            className="input-primary"
-            placeholder="Agent"
-          />
-        </div>
-        {/* Status */}
-        <div className="flex flex-col 1">
-          <label className="text-sm">Status</label>
-          {/* <select
+      <form onSubmit={handleSubmit(onSearch)}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 md:gap-x-16 lg:gap-x-24 gap-y-4 text-text-secondary">
+          {/* First Name */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">First Name</label>
+            <input
+              {...register("firstName", { required: "First Name is required" })}
+              // value={criteria.firstName || ""}
+              // onChange={handleChange("firstName")}
+              className="input-primary"
+              placeholder="First Name"
+            />
+            {errors.firstName && (
+              <span className="text-red-500 text-sm">
+                {errors.firstName.message}
+              </span>
+            )}
+          </div>
+          {/* Last Name */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Last Name</label>
+            <input
+              {...register("lastName", { required: "Last Name is required" })}
+              // value={criteria.lastName || ""}
+              // onChange={handleChange("lastName")}
+              className="input-primary"
+              placeholder="Last Name"
+            />
+            {errors.lastName && (
+              <span className="text-red-500 text-sm">
+                {errors.lastName.message}
+              </span>
+            )}
+          </div>
+          {/* Date of Birth */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Date of Birth</label>
+            <input
+              type="date"
+              {...register("dateOfBirth", {
+                required: "Date of Birth is required",
+              })}
+              // value={criteria.dateOfBirth || ""}
+              // onChange={handleChange("dateOfBirth")}
+              className="input-primary"
+              placeholder="Date of Birth"
+            />
+            {errors.dateOfBirth && (
+              <span className="text-red-500 text-sm">
+                {errors.dateOfBirth.message}
+              </span>
+            )}
+          </div>
+          {/* Policy Number */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Policy Number</label>
+            <input
+              {...register("policyNumber", {
+                required: "Policy Number is required",
+              })}
+              // value={criteria.policyNumber || ""}
+              // onChange={handleChange("policyNumber")}
+              className="input-primary"
+              placeholder="Policy Number"
+            />
+            {errors.policyNumber && (
+              <span className="text-red-500 text-sm">
+                {errors.policyNumber.message}
+              </span>
+            )}
+          </div>
+          {/* Phone Number */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Phone Number</label>
+            <input
+              {...register("phoneNumber", {
+                required: "Phone Number is required",
+              })}
+              // value={criteria.phoneNumber || ""}
+              // onChange={handleChange("phoneNumber")}
+              className="input-primary"
+              placeholder="Phone Number"
+            />
+            {errors.phoneNumber && (
+              <span className="text-red-500 text-sm">
+                {errors.phoneNumber.message}
+              </span>
+            )}
+          </div>
+          {/* Email */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Email</label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email address",
+                },
+              })}
+              // value={criteria.email || ""}
+              // onChange={handleChange("email")}
+              className="input-primary"
+              placeholder="Email"
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          {/* Sale Date From */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Sale Date From</label>
+            <input
+              type="date"
+              {...register("saleDateFrom", {
+                required: "Sale Date From is required",
+              })}
+              // value={criteria.saleDateFrom || ""}
+              // onChange={handleChange("saleDateFrom")}
+              className="input-primary"
+            />
+            {errors.saleDateFrom && (
+              <span className="text-red-500 text-sm">
+                {errors.saleDateFrom.message}
+              </span>
+            )}
+          </div>
+          {/* Sale Date To */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Sale Date To</label>
+            <input
+              type="date"
+              {...register("saleDateTo", {
+                required: "Sale Date To is required",
+              })}
+              // value={criteria.saleDateTo || ""}
+              // onChange={handleChange("saleDateTo")}
+              className="input-primary"
+            />
+            {errors.saleDateTo && (
+              <span className="text-red-500 text-sm">
+                {errors.saleDateTo.message}
+              </span>
+            )}
+          </div>
+          {/* Effective Date From */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Effective Date From</label>
+            <input
+              type="date"
+              {...register("effectiveDateFrom", {
+                required: "Effective Date From is required",
+              })}
+              // value={criteria.effectiveDateFrom || ""}
+              // onChange={handleChange("effectiveDateFrom")}
+              className="input-primary"
+            />
+            {errors.effectiveDateFrom && (
+              <span className="text-red-500 text-sm">
+                {errors.effectiveDateFrom.message}
+              </span>
+            )}
+          </div>
+          {/* Effective Date To */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Effective Date To</label>
+            <input
+              type="date"
+              {...register("effectiveDateTo", {
+                required: "Effective Date To is required",
+              })}
+              // value={criteria.effectiveDateTo || ""}
+              // onChange={handleChange("effectiveDateTo")}
+              className="input-primary"
+            />
+            {errors.effectiveDateTo && (
+              <span className="text-red-500 text-sm">
+                {errors.effectiveDateTo.message}
+              </span>
+            )}
+          </div>
+          {/* Application ID */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Application ID</label>
+            <input
+              {...register("applicationId", {
+                required: "Application ID is required",
+              })}
+              // value={criteria.applicationId || ""}
+              // onChange={handleChange("applicationId")}
+              className="input-primary"
+              placeholder="Application ID"
+            />
+            {errors.applicationId && (
+              <span className="text-red-500 text-sm">
+                {errors.applicationId.message}
+              </span>
+            )}
+          </div>
+          {/* Agent */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Agent</label>
+            <input
+              {...register("agent", { required: "Agent is required" })}
+              // value={criteria.agent || ""}
+              // onChange={handleChange("agent")}
+              className="input-primary"
+              placeholder="Agent"
+            />
+            {errors.agent && (
+              <span className="text-red-500 text-sm">
+                {errors.agent.message}
+              </span>
+            )}
+          </div>
+          {/* Status */}
+          <div className="flex flex-col 1">
+            <label className="text-sm">Status</label>
+            {/* <select
             value={criteria.status || "All"}
             onChange={(e) =>
               setCriteria((prev) => ({
@@ -248,49 +358,50 @@ const PoliciesSearch: React.FC = () => {
             <option>Expired</option>
             <option>Cancelled</option>
           </select> */}
-          <div className="relative bg-white">
-            <button
-              type="button"
-              className="w-full border border-inputBorder py-2 sm:py-3 px-4 focus:border-0 focus:ring-1 focus:ring-primary capitalize flex items-center justify-between text-left text-text-light cursor-pointer"
-              onClick={() => setIsSelectStatusOpen((prev) => !prev)}
-            >
-              <span className="capitalize">
-                {criteria.status || "All"}
-              </span>
-              <FaAngleDown
-                className={`ml-2 cusor-pointer transition-transform ${
-                  isSelectStatusOpen ? "rotate-180" : ""
-                }`}
-              />
-            </button>
+            <div className="relative bg-white">
+              <button
+                type="button"
+                className="w-full border border-inputBorder py-2 sm:py-3 px-4 focus:border-0 focus:ring-1 focus:ring-primary capitalize flex items-center justify-between text-left text-text-light cursor-pointer"
+                onClick={() => setIsSelectStatusOpen((prev) => !prev)}
+              >
+                <span className="capitalize">{criteria.status || "All"}</span>
+                <FaAngleDown
+                  className={`ml-2 cusor-pointer transition-transform ${
+                    isSelectStatusOpen ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
 
-            {isSelectStatusOpen && (
-              <div className="absolute mt-[2px] top-full left-0 w-full bg-white border border-inputBorder shadow-md z-10 max-h-60 overflow-y-auto custom-scrollbar3 pr-[2px]">
-                {status.map((s,i) => (
-                  <div
-                    key={i}
-                    className={`px-4 py-2 hover:bg-gray-200 text-text-light cursor-pointer capitalize ${
-                      criteria.status === s
-                        ? "bg-primary text-white hover:bg-gray-200 hover:text-text-light"
-                        : ""
-                    }`}
-                    onClick={() => {
-                      setCriteria((prev) => ({
-                        ...prev,
-                        status: s,
-                      }))
-                      setIsSelectStatusOpen(false);
-                      // setCategoryError(null);
-                    }}
-                  >
-                    {s}
-                  </div>
-                ))}
-              </div>
+              {isSelectStatusOpen && (
+                <div className="absolute mt-[2px] top-full left-0 w-full bg-white border border-inputBorder shadow-md z-10 max-h-60 overflow-y-auto custom-scrollbar3 pr-[2px]">
+                  {status.map((s, i) => (
+                    <div
+                      key={i}
+                      className={`px-4 py-2 hover:bg-gray-200 text-text-light cursor-pointer capitalize ${
+                        criteria.status === s
+                          ? "bg-primary text-white hover:bg-gray-200 hover:text-text-light"
+                          : ""
+                      }`}
+                      onClick={() => {
+                        setCriteria((prev) => ({ ...prev, status: s }));
+                        setValue("status", s, { shouldValidate: true });
+                        setIsSelectStatusOpen(false);
+                      }}
+                    >
+                      {s}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            {errors.status && (
+              <p className="text-red-500 text-sm text-sm mt-1">
+                {errors.status.message}
+              </p>
             )}
           </div>
         </div>
-      </div>
+      </form>
 
       {/* Product list */}
       <div className="mt-6">
@@ -322,7 +433,11 @@ const PoliciesSearch: React.FC = () => {
       </div>
 
       <div className="flex justify-center mt-6">
-        <button onClick={onSearch} disabled={loading} className="btn-primary">
+        <button
+          onClick={handleSubmit(onSearch)}
+          disabled={loading}
+          className="btn-primary"
+        >
           {loading ? "Searching..." : "Search Policies"}
         </button>
       </div>
