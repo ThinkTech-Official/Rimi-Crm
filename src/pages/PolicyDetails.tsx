ca7810db95af3583666ed58bf37a048593b780bc
ThinkTech <tech@thethinktech.com>
2026-03-12 18:57:29 +0530

    fix the calculate date function in policy split modal and policy details

--- src/pages/PolicyDetails.tsx
+++ src/pages/PolicyDetails.tsx
@@ -270,7 +270,7 @@ const PolicyDetailsPage: React.FC = () => {
     const startDate = new Date(start);
     const endDate = new Date(end);
     const diffMs = endDate.getTime() - startDate.getTime();
-    return Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    return Math.floor(diffMs / (1000 * 60 * 60 * 24)) + 1;
   };
 
   const validateModification = (): {
