2b053fc0893718fc1d12a89ae6256d752922747d
ThinkTech <tech@thethinktech.com>
2026-03-17 23:33:49 +0530

    logout never calls /auth/logout endpoint

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,32 @@
+// import { useAppDispatch, useAppSelector } from '../hooks/useReduxHooks';
+// import { loginUser, logout } from '../features/authSlice';
+
+// export const useAuth = () => {
+//   const dispatch = useAppDispatch();
+//   const { token, loading, error, userType, fullName, agentCode } = useAppSelector((state) => state.auth);
+
+//   const login = (email: string, password: string) => {
+//     return dispatch(loginUser({ email, password }));
+//   };
+
+//   return {
+//     token,
+//     userType,
+//     fullName,
+//     agentCode,
+//     loading,
+//     error,
+//     login,
+//     logout: () => dispatch(logout()),
+//   };
+// };
+
+
+// ==================
+
+import { loginUser, logoutUser } from '../features/authSlice'; 
+import { clearVerificationData } from '../features/verificationSlice'; 
 import { useAppDispatch, useAppSelector } from '../hooks/useReduxHooks';
-import { loginUser, logout } from '../features/authSlice';
 
 export const useAuth = () => {
   const dispatch = useAppDispatch();
@@ -7,6 +34,11 @@ export const useAuth = () => {
 
   const login = (email: string, password: string) => {
     return dispatch(loginUser({ email, password }));
+  };
+
+  const logout = async () => {
+    await dispatch(logoutUser()); 
+    dispatch(clearVerificationData()); 
   };
 
   return {
@@ -17,6 +49,6 @@ export const useAuth = () => {
     loading,
     error,
     login,
-    logout: () => dispatch(logout()),
+    logout,
   };
 };
